3e7c24ba0bfbae85df7053e0a15394a5731db975
Jared King <jared.m.king@gmail.com>
2015-09-03 12:44:47 -0700

    Updated version to 0.4.0

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'apache2'
 description 'Installs/Configures SSSD for AD integration'
 long_description 'Installs/Configures SSSD for AD integration'
 supports 'ubuntu'
-version '0.3.0'
+version '0.4.0'
 
 depends 'chef-vault', '~> 1.0'
 depends 'samba', '~> 0.12'
